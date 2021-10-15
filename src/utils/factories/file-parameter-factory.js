e01692347798b86ac89da69c6492721b7cb67371
Tim Basti <73779054+timbasti@users.noreply.github.com>
2021-10-15 14:48:56 +0200

    Small change and git ignore entry

--- src/utils/factories/file-parameter-factory.js
+++ src/utils/factories/file-parameter-factory.js
@@ -4,15 +4,11 @@ import {EnrichedFileInput} from '../../components/input-fields';
 import {requiredValueErrorMessage} from '../error-messages';
 
 export const createFileParameterInput = (parameter, parentName) => {
-    const accept = parameter?.acceptedExtensions
-        .split(',')
-        .map((extension) => (extension.startsWith('.') ? extension : `.${extension}`))
-        .join(',') || '';
     const requiredRule = parameter.required ? requiredValueErrorMessage : false;
     const name = parentName ? `${parentName}.${parameter.name}` : parameter.name;
     return (
         <EnrichedFileInput
-            accept={accept}
+            accept={parameter.acceptedExtensions}
             helperText={parameter.comment}
             label={parameter.name}
             name={name}
